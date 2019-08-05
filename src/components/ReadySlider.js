41e61129032af6819b2711fd333e0bf245e716c9
dbond762 <dbond762@gmail.com>
2019-08-05 22:15:43 +0300

    box animation

--- src/components/ReadySlider.js
+++ src/components/ReadySlider.js
@@ -11,33 +11,41 @@ function ReadySlider() {
       <Slide className="slide-1">
         <h1 className="slide-1__header">Всегда ли цели терапии СД2 на поверхности?</h1>
         <div className="box box-1 slide-1__box-1">
-          <div className="box__elipse-1 box-1__elipse">
-            <div className="box__elipse-2">
-              <div className="box__elipse-3"></div>
+          <div className="box__elipse box-1__elipse">
+            <div className="box__elipse-1">
+              <div className="box__elipse-2">
+                <div className="box__elipse-3"></div>
+              </div>
             </div>
           </div>
           <span className="box__text box-1__text">Цель по HbA1c</span>
         </div>
         <div className="box box-2 slide-1__box-2">
-          <div className="box__elipse-1 box-2__elipse">
-            <div className="box__elipse-2">
-              <div className="box__elipse-3"></div>
+          <div className="box__elipse box-2__elipse">
+            <div className="box__elipse-1">
+              <div className="box__elipse-2">
+                <div className="box__elipse-3"></div>
+              </div>
             </div>
           </div>
           <span className="box__text box-2__text">Гипогликемия</span>
         </div>
         <div className="box box-3 slide-1__box-3">
-          <div className="box__elipse-1 box-3__elipse">
-            <div className="box__elipse-2">
-              <div className="box__elipse-3"></div>
+          <div className="box__elipse box-3__elipse">
+            <div className="box__elipse-1">
+              <div className="box__elipse-2">
+                <div className="box__elipse-3"></div>
+              </div>
             </div>
           </div>
           <span className="box__text box-3__text">Осложнения СД</span>
         </div>
         <div className="box box-4 slide-1__box-4">
-          <div className="box__elipse-1 box-4__elipse">
-            <div className="box__elipse-2">
-              <div className="box__elipse-3"></div>
+          <div className="box__elipse box-4__elipse">
+            <div className="box__elipse-1">
+              <div className="box__elipse-2">
+                <div className="box__elipse-3"></div>
+              </div>
             </div>
           </div>
           <span className="box__text box-4__text">СС риски</span>
@@ -50,7 +58,7 @@ function ReadySlider() {
       <Slide className="slide-3">
         <div className="slide-3__ice"></div>
         <h2 className="slide-3__header">Звенья патогенеза СД2</h2>
-        <Scheme/>
+        <Scheme />
       </Slide>
     </Slider>
   );
